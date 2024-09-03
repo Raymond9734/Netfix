e03e2dc478ca8b29dd041029ed6e9734d76faf30
Raymond9734 <rymadara97@gmail.com>
2024-09-04 01:24:29 +0300

    Update URL patterns
    - Renamed `company/` to `company_registration/` for clarity and consistency.
    - Commented out `customer/` path, which is not currently in use.
    - Updated view function for `register_company` to `v.register_company` for correct reference.
    - No changes made to `login/` URL pattern.

--- users/urls.py
+++ users/urls.py
@@ -6,7 +6,7 @@ from . import views as v
 
 urlpatterns = [
     path('', v.register, name='register'),
-    path('company/', v.CompanySignUpView.as_view(), name='register_company'),
-    path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
+    path('company_registration/', v.register_company, name='register_company'),
+    # path('customer/', v.CustomerSignUpView.as_view(), name='register_customer'),
     path('login/', v.LoginUserView, name='login_user')
 ]
