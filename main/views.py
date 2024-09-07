f5a8330c4f8f95acf5944428d55683cc55bc23e9
Raymond9734 <rymadara97@gmail.com>
2024-09-07 09:43:17 +0300

    Enhance home view to include services in context
    - Updated `home` view to query the `Service` model and pass all services as context to the template.
    - Added `most_requested_services` to the context, which currently includes all services.
    - No changes made to the `logout` view.

--- main/views.py
+++ main/views.py
@@ -2,8 +2,22 @@ from django.shortcuts import render
 from django.contrib.auth import logout as django_logout
 
 
+from django.shortcuts import render
+from services.models import Service
+
+
 def home(request):
-    return render(request, "main/home.html", {})
+    # Query the Service model to get all services
+    services = Service.objects.all()
+
+    # You might want to query the most requested services separately
+    # For demonstration, we'll assume you want to show all services as most requested services
+    most_requested_services = services
+
+    # Pass services and most_requested_services to the template context
+    context = {"services": services, "most_requested_services": most_requested_services}
+
+    return render(request, "main/home.html", context)
 
 
 def logout(request):
