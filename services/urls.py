6bc67eba9a7011d8f1d266afdb31c3e91c932878
Raymond9734 <rymadara97@gmail.com>
2024-09-10 22:24:00 +0300

    Updated service URLs to include detailed request_service path
    - Modified `request_service` URL pattern to include `company_name` and `service_id` for more specific routing.
    - Ensured URL names and patterns are consistent with the updated views.

--- services/urls.py
+++ services/urls.py
@@ -2,11 +2,15 @@ from django.urls import path
 from . import views as v
 
 
-app_name ='services'
+app_name = "services"
 urlpatterns = [
-    path('', v.service_list, name='services_list'),
-    path('create/', v.create, name='services_create'),
-    path('<int:id>', v.index, name='index'),
-    path('<int:id>/request_service/', v.request_service, name='request_service'),
-    path('<slug:field>/', v.service_field, name='services_field'),
+    path("", v.service_list, name="services_list"),
+    path("create/", v.create, name="services_create"),
+    path("<int:id>", v.index, name="index"),
+    path(
+        "request_service/<slug:company_name>/<int:service_id>/",
+        v.request_service,
+        name="request_service",
+    ),
+    path("<slug:field>/", v.service_field, name="services_field"),
 ]
