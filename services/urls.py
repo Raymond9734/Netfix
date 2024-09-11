e23d6e8fa72279923777ca1bf15a6923ef54404d
Raymond9734 <rymadara97@gmail.com>
2024-09-12 00:01:15 +0300

    Update service URLs
    - Renamed `services_list` to `service_list` and updated URL path accordingly.
    - Removed redundant or unused paths.

--- services/urls.py
+++ services/urls.py
@@ -4,7 +4,6 @@ from . import views as v
 
 app_name = "services"
 urlpatterns = [
-    path("", v.service_list, name="services_list"),
     path("create/", v.create, name="services_create"),
     path("<int:id>", v.index, name="index"),
     path(
@@ -12,5 +11,5 @@ urlpatterns = [
         v.request_service,
         name="request_service",
     ),
-    path("<slug:field>/", v.service_field, name="services_field"),
+    path("service_list/", v.services_list, name="service_list"),
 ]
